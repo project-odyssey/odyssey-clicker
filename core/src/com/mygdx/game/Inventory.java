30298fd1aae3a37ed969d411e231a92a3d7165f6
gmcfeeters3345 <guy.mckrone@gmail.com>
2017-10-16 13:52:40 -0700

    Added Array

--- core/src/com/mygdx/game/Inventory.java
+++ core/src/com/mygdx/game/Inventory.java
@@ -5,4 +5,14 @@ package com.mygdx.game;
  */
 
 public class Inventory {
+    public void Inventory() {
+
+        int[] inventory = new int[10];
+
+        for(int i=0; i < inventory.length; i++){
+
+        }
+    }
+
+
 }
--- core/src/com/mygdx/game/PlayScreen.java
+++ core/src/com/mygdx/game/PlayScreen.java
@@ -1,8 +1,12 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.input.GestureDetector;
+
 /**
  * Created by gmcfeeters3345 on 10/16/2017.
  */
 
 public class PlayScreen {
+
+    
 }
