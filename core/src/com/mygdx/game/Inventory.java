ef11f1c246ba6f7299c21f3f18469901a4bc4cee
gmcfeeters3345 <guy.mckrone@gmail.com>
2017-10-18 13:32:08 -0700

    Added screen switching code

--- core/src/com/mygdx/game/Inventory.java
+++ core/src/com/mygdx/game/Inventory.java
@@ -1,11 +1,15 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by gmcfeeters3345 on 10/16/2017.
  */
 
-public class Inventory {
-    public void Inventory() {
+public class Inventory implements Screen{
+
+    public Inventory(Game game) {
 
         int[] inventory = new int[10];
 
@@ -15,4 +19,38 @@ public class Inventory {
     }
 
 
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
--- core/src/com/mygdx/game/PlayScreen.java
+++ core/src/com/mygdx/game/PlayScreen.java
@@ -1,12 +1,66 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by gmcfeeters3345 on 10/16/2017.
  */
 
-public class PlayScreen {
+public class PlayScreen extends Game implements GestureDetector.GestureListener {
+    int[] inventory = new int[10];
 
-    
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        inventory[0]++;
+        return true;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
+    }
 }
