7825135d30984bc7f747808dc44d5af0d76fff14
Natty Leof <NLeof4499@student.pps.net>
2017-10-18 13:49:21 -0700

    added an image and positioned it

--- core/src/com/mygdx/game/ProjectOdyssey.java
+++ core/src/com/mygdx/game/ProjectOdyssey.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sun.javafx.scene.paint.GradientUtils;
+
+import org.lwjgl.opengl.Display;
 
 public class ProjectOdyssey extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("play.jpg");
 		//41
 	}
 
@@ -22,7 +25,7 @@ public class ProjectOdyssey extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1); //White background color
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 300, 300, 150, 100, 300, 200, 1, 1, 90, 1, 1, 300, 200, false, false); //https://stackoverflow.com/questions/24748350/libgdx-rotate-a-texture-when-drawing-it-with-spritebatch
 		batch.end();
 	}
 	
