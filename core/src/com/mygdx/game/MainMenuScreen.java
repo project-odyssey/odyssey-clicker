4d20c2e4f46cfcb21faa109bb6bb9fd6634324e1
Ryan Kinnucan <RKinnucan2330@student.pps.net>
2017-10-24 13:41:12 -0700

    nothing

---
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -0,0 +1,50 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by gmcfeeters3345 on 10/16/2017.
+ */
+
+public class MainMenuScreen implements Screen{
+    public MainMenuScreen(Game projectOdyssey) {
+
+        
+    }
+
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
