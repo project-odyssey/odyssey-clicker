45ab988151b8fc4782fa340961b312525986e363
gmcfeeters3345 <guy.mckrone@gmail.com>
2017-10-30 13:52:38 -0700

    Changed stuff in inventory, didnt work

--- core/src/com/mygdx/game/Inventory.java
+++ core/src/com/mygdx/game/Inventory.java
@@ -1,9 +1,17 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /**
  * Created by gmcfeeters3345 on 10/16/2017.
@@ -11,7 +19,44 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Inventory extends Game implements Screen, GestureDetector.GestureListener{
     int ice;
-    public Inventory(Game game) {
+    private Stage stage;
+    private SpriteBatch batch;
+    private ImageButton iceButton;
+    public Inventory(final ProjectOdyssey game) {
+        stage = new Stage();
+        batch = new SpriteBatch();
+
+        //Button skin
+        Skin iceButtonSkin = new Skin();
+        iceButtonSkin.add("iceButton", new Texture("buttons/ice.png"));
+
+        //Button Style
+        ImageButton.ImageButtonStyle iceButtonStyle = new ImageButton.ImageButtonStyle();
+        iceButtonStyle.imageUp = iceButtonSkin.getDrawable("iceButton"); // Unpressed
+        iceButtonStyle.imageDown = iceButtonSkin.getDrawable("iceButton"); // Pressed
+
+        //Ice button
+        iceButton = new ImageButton(iceButtonStyle);
+        int buttonSize = (int) (100 * Gdx.graphics.getDensity());
+        iceButton.setSize(buttonSize, buttonSize);
+        int width = (int) ((Gdx.graphics.getWidth() - iceButton.getWidth())/2);
+        int height = (int) ((Gdx.graphics.getHeight() - iceButton.getHeight())/4);
+        iceButton.setBounds(width, height, iceButton.getWidth(), iceButton.getHeight());
+        iceButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                //game.setScreen(new PlayScreen(game)); // Switch screen to game state
+                //iceButton.setDisabled(true);
+
+            }
+        });
+        stage.addActor(iceButton);
+        System.out.println("onetwofour");
+        Gdx.gl.glClearColor(1, 1, 1, 1); //White background color
 
         int[] inventory = new int[10];
 
